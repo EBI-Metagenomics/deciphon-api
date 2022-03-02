c37b9cad1206b90f5638a61104b1a57f167b0532
Danilo Horta <danilo.horta@pm.me>
2022-03-02 23:27:37 +0000

    tests

--- deciphon_api/test/test_dbs.py
+++ deciphon_api/test/test_dbs.py
@@ -1,12 +1,25 @@
+import os
+import shutil
+from pathlib import Path
+
 from fastapi.testclient import TestClient
 
-from deciphon_api import app
 
-client = TestClient(app)
+def test_httppost_dbs(tmp_path: Path):
+    os.chdir(tmp_path)
 
+    import deciphon_api.data as data
+    from deciphon_api import app
 
-def test_httppost_dbs():
-    response = client.post("/dbs/", json=["filename", "minifam.hmm"])
-    # ?filename=pfam24.dcp
-    assert response.status_code == 500
-    assert response.json() == {"msg": "Hello World"}
+    minifam = data.filepath(data.FileName.minifam)
+    shutil.copy(minifam, os.getcwd())
+    print(os.getcwd())
+
+    with TestClient(app) as client:
+        response = client.post("/dbs/", json={"filename": "minifam.hmm"})
+        assert response.status_code == 201
+        assert response.json() == {
+            "filename": "minifam.hmm",
+            "id": 1,
+            "xxh64": -8445839449675891342,
+        }

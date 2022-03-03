39be0745f97d3c95f497ebe174adfe2a8a8c86b4
Danilo Horta <danilo.horta@pm.me>
2022-03-03 13:17:39 +0000

    dbs mostly tested

--- deciphon_api/api/httpget_dbs_xxx.py
+++ deciphon_api/api/httpget_dbs_xxx.py
@@ -1,3 +1,5 @@
+from typing import List
+
 from fastapi import APIRouter
 from starlette.status import (
     HTTP_200_OK,
@@ -8,7 +10,7 @@ from starlette.status import (
 from .._types import ErrorResponse
 from ..csched import ffi, lib
 from ..db import DB
-from ..exception import create_exception
+from ..exception import EINVALException, create_exception
 from ..rc import RC
 
 router = APIRouter()
@@ -17,7 +19,7 @@ router = APIRouter()
 @router.get(
     "/dbs/{db_id}",
     summary="get database",
-    response_model=DB,
+    response_model=List[DB],
     status_code=HTTP_200_OK,
     responses={
         HTTP_404_NOT_FOUND: {"model": ErrorResponse},
@@ -32,9 +34,9 @@ def httpget_dbs_xxx(db_id: int):
     assert rc != RC.END
 
     if rc == RC.NOTFOUND:
-        return []
+        raise EINVALException(HTTP_404_NOT_FOUND, "db not found")
 
     if rc != RC.OK:
         raise create_exception(HTTP_500_INTERNAL_SERVER_ERROR, rc)
 
-    return DB.from_cdata(cdb)
+    return [DB.from_cdata(cdb)]
--- deciphon_api/test/test_dbs.py
+++ deciphon_api/test/test_dbs.py
@@ -13,12 +13,15 @@ def test_httppost_dbs(app: FastAPI):
 
     with TestClient(app) as client:
         response = client.post("/dbs/", json={"filename": "minifam.hmm"})
-        assert response.status_code == 201
-        assert response.json() == {
+
+    assert response.status_code == 201
+    assert response.json() == [
+        {
             "filename": "minifam.hmm",
             "id": 1,
             "xxh64": -8445839449675891342,
         }
+    ]
 
 
 def test_httppost_dbs_notfound(app: FastAPI):
@@ -28,4 +31,32 @@ def test_httppost_dbs_notfound(app: FastAPI):
         assert response.json() == {
             "rc": "einval",
             "msg": "file not found",
+        }
+
+
+def test_httpget_dbs(app: FastAPI):
+    minifam = data.filepath(data.FileName.minifam)
+    shutil.copy(minifam, os.getcwd())
+
+    with TestClient(app) as client:
+        response = client.post("/dbs/", json={"filename": "minifam.hmm"})
+        assert response.status_code == 201
+
+        response = client.get("/dbs/1")
+        assert response.json() == [
+            {
+                "filename": "minifam.hmm",
+                "id": 1,
+                "xxh64": -8445839449675891342,
+            }
+        ]
+
+
+def test_httpget_dbs_notfound(app: FastAPI):
+    with TestClient(app) as client:
+        response = client.get("/dbs/1")
+        assert response.status_code == 404
+        assert response.json() == {
+            "rc": "einval",
+            "msg": "db not found",
         }

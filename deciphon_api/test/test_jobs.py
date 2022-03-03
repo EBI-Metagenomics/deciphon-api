526caf2ce09b8c2feb92d3833d9695f58bfe89b6
Danilo Horta <danilo.horta@pm.me>
2022-03-03 15:00:27 +0000

    next pend

--- deciphon_api/test/test_jobs.py
+++ deciphon_api/test/test_jobs.py
@@ -21,6 +21,8 @@ def test_httppost_jobs(app: FastAPI):
 
     with TestClient(app) as client:
         response = client.post("/dbs/", json={"filename": "minifam.hmm"})
+        assert response.status_code == 201
+
         response = client.post("/jobs/", json=JobPost.example().dict())
         assert response.status_code == 201
         json = response.json()
@@ -47,8 +49,30 @@ def test_httpget_jobs_next_pend_empty(app: FastAPI):
         assert response.json() == []
 
 
-# def test_httpget_jobs_next_pend(app: FastAPI):
-#     with TestClient(app) as client:
-#         response = client.get("/jobs/next_pend")
-#         assert response.status_code == 200
-#         assert response.json() == []
+def test_httpget_jobs_next_pend(app: FastAPI):
+    minifam = data.filepath(data.FileName.minifam)
+    shutil.copy(minifam, os.getcwd())
+
+    with TestClient(app) as client:
+        response = client.post("/dbs/", json={"filename": "minifam.hmm"})
+        assert response.status_code == 201
+        response = client.post("/jobs/", json=JobPost.example().dict())
+        assert response.status_code == 201
+        response = client.get("/jobs/next_pend")
+        assert response.status_code == 200
+
+        json = response.json()
+        assert "submission" in json[0]
+        del json[0]["submission"]
+        assert json == [
+            {
+                "db_id": 1,
+                "error": "",
+                "exec_ended": 0,
+                "exec_started": 0,
+                "hmmer3_compat": False,
+                "id": 1,
+                "multi_hits": True,
+                "state": "pend",
+            },
+        ]

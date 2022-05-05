715788fd85e416da9900274c9916a61a083f56b1
Danilo Horta <danilo.horta@pm.me>
2022-05-05 14:46:25 +0100

    trigger

--- deciphon_api/sched/interface.h
+++ deciphon_api/sched/interface.h
@@ -71,6 +71,90 @@ enum sched_rc
     SCHED_FAIL_ROLLBACK_TRANSACTION,
 };
 
+/* --- structs.h file --- */
+
+struct sched_scan
+{
+    int64_t id;
+    int64_t db_id;
+
+    int multi_hits;
+    int hmmer3_compat;
+
+    int64_t job_id;
+};
+
+struct sched_hmm
+{
+    int64_t id;
+    int64_t xxh3;
+    char filename[SCHED_FILENAME_SIZE];
+    int64_t job_id;
+};
+
+struct sched_db
+{
+    int64_t id;
+    int64_t xxh3;
+    char filename[SCHED_FILENAME_SIZE];
+    int64_t hmm_id;
+};
+
+struct sched_prod
+{
+    int64_t id;
+
+    int64_t scan_id;
+    int64_t seq_id;
+
+    char profile_name[SCHED_PROFILE_NAME_SIZE];
+    char abc_name[SCHED_ABC_NAME_SIZE];
+
+    double alt_loglik;
+    double null_loglik;
+
+    char profile_typeid[SCHED_PROFILE_TYPEID_SIZE];
+    char version[SCHED_VERSION_SIZE];
+
+    char match[SCHED_MATCH_SIZE];
+};
+
+enum sched_job_type
+{
+    SCHED_SCAN,
+    SCHED_HMM
+};
+
+enum sched_job_state
+{
+    SCHED_PEND,
+    SCHED_RUN,
+    SCHED_DONE,
+    SCHED_FAIL
+};
+
+struct sched_job
+{
+    int64_t id;
+    int type;
+
+    char state[SCHED_JOB_STATE_SIZE];
+    int progress;
+    char error[SCHED_JOB_ERROR_SIZE];
+
+    int64_t submission;
+    int64_t exec_started;
+    int64_t exec_ended;
+};
+
+struct sched_seq
+{
+    int64_t id;
+    int64_t scan_id;
+    char name[SCHED_SEQ_NAME_SIZE];
+    char data[SCHED_SEQ_SIZE];
+};
+
 /* --- sched.h file --- */
 
 struct sched_health
@@ -200,88 +284,4 @@ enum sched_rc sched_scan_get_all(sched_scan_set_func_t, struct sched_scan *,
 /* --- error.h file --- */
 
 char const *sched_error_string(enum sched_rc rc);
-
-/* --- structs.h file --- */
-
-struct sched_scan
-{
-    int64_t id;
-    int64_t db_id;
-
-    int multi_hits;
-    int hmmer3_compat;
-
-    int64_t job_id;
-};
-
-struct sched_hmm
-{
-    int64_t id;
-    int64_t xxh3;
-    char filename[SCHED_FILENAME_SIZE];
-    int64_t job_id;
-};
-
-struct sched_db
-{
-    int64_t id;
-    int64_t xxh3;
-    char filename[SCHED_FILENAME_SIZE];
-    int64_t hmm_id;
-};
-
-struct sched_prod
-{
-    int64_t id;
-
-    int64_t scan_id;
-    int64_t seq_id;
-
-    char profile_name[SCHED_PROFILE_NAME_SIZE];
-    char abc_name[SCHED_ABC_NAME_SIZE];
-
-    double alt_loglik;
-    double null_loglik;
-
-    char profile_typeid[SCHED_PROFILE_TYPEID_SIZE];
-    char version[SCHED_VERSION_SIZE];
-
-    char match[SCHED_MATCH_SIZE];
-};
-
-enum sched_job_type
-{
-    SCHED_SCAN,
-    SCHED_HMM
-};
-
-enum sched_job_state
-{
-    SCHED_PEND,
-    SCHED_RUN,
-    SCHED_DONE,
-    SCHED_FAIL
-};
-
-struct sched_job
-{
-    int64_t id;
-    int type;
-
-    char state[SCHED_JOB_STATE_SIZE];
-    int progress;
-    char error[SCHED_JOB_ERROR_SIZE];
-
-    int64_t submission;
-    int64_t exec_started;
-    int64_t exec_ended;
-};
-
-struct sched_seq
-{
-    int64_t id;
-    int64_t scan_id;
-    char name[SCHED_SEQ_NAME_SIZE];
-    char data[SCHED_SEQ_SIZE];
-};
 

42519ad51cdf82534c2c7f18b91b9b8674d86ffd
Danilo Horta <danilo.horta@pm.me>
2022-12-13 14:01:38 +0000

    send job_id around for scan as well

--- deciphon_api/api/scans_pub.py
+++ deciphon_api/api/scans_pub.py
@@ -56,8 +56,6 @@ async def upload_scan(
         if not db:
             raise NotFoundException(DB)
 
-        hmm_id = db.hmm.id
-        hmm_file = db.hmm.filename
         session.add(scan)
         session.commit()
         session.refresh(scan)
@@ -70,10 +68,11 @@ async def upload_scan(
             producer.publish(
                 {
                     "id": scan.id,
-                    "hmm_id": hmm_id,
-                    "hmm_file": hmm_file,
+                    "hmm_id": db.hmm.id,
+                    "hmm_file": db.hmm.filename,
                     "db_id": db_id,
                     "db_file": db.filename,
+                    "job_id": scan.job.id,
                 },
                 exchange=exchange,
                 routing_key="scan",

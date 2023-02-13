8677d61bc76f32a9144a2a7429ce7391be73c077
Danilo Horta <danilo.horta@pm.me>
2023-02-13 14:23:38 +0000

    gff_writer, make_hits, snap_gff

---
+++ deciphon_api/make_hits.py
@@ -0,0 +1,70 @@
+import dataclasses
+from typing import List
+
+from deciphon_api.models import Prod
+
+__all__ = ["make_hits"]
+
+
+@dataclasses.dataclass
+class Match:
+    state: str
+    query: str
+    codon: str
+    amino: str
+
+    def get(self, field: str):
+        return dataclasses.asdict(self)[field]
+
+
+@dataclasses.dataclass
+class Hit:
+    id: int
+    name: str
+    prod_id: int
+    evalue_log: float
+    matchs: List[Match] = dataclasses.field(default_factory=lambda: [])
+    feature_start: int = 0
+    feature_end: int = 0
+
+
+def is_core_state(state: str):
+    return state.startswith("M") or state.startswith("I") or state.startswith("D")
+
+
+def make_hits(prod: Prod) -> List[Hit]:
+    hits: List[Hit] = []
+
+    hit_start = 0
+    hit_end = 0
+    offset = 0
+    hit_start_found = False
+    hit_end_found = False
+
+    for query_match in prod.match.split(";"):
+        query, state, codon, amino = query_match.split(",")
+
+        if not hit_start_found and is_core_state(state):
+            hit_start = offset
+            hit_start_found = True
+            evalue_log = prod.evalue_log
+            name = prod.seq.name
+            assert prod.id
+            hits.append(Hit(len(hits) + 1, name, prod.id, evalue_log))
+
+        if hit_start_found and not is_core_state(state):
+            hit_end = offset + len(query)
+            hit_end_found = True
+
+        if hit_start_found and not hit_end_found:
+            hits[-1].matchs.append(Match(state[0], query, codon, amino))
+
+        if hit_end_found:
+            hits[-1].feature_start = hit_start
+            hits[-1].feature_end = hit_end
+            hit_start_found = False
+            hit_end_found = False
+
+        offset += len(query)
+
+    return hits
---
+++ deciphon_api/snap_gff.py
@@ -0,0 +1,44 @@
+from Bio.Seq import Seq as BioSeq
+from Bio.SeqFeature import FeatureLocation, SeqFeature
+from Bio.SeqRecord import SeqRecord
+
+from deciphon_api.gff_writer import GFFWriter
+from deciphon_api.make_hits import make_hits
+from deciphon_api.models import Snap
+
+EPSILON = "0.01"
+
+__all__ = ["snap_gff"]
+
+
+def snap_gff(snap: Snap):
+    hits = [i for prod in snap.prods for i in make_hits(prod)]
+
+    if len(snap.prods) == 0:
+        return "##gff-version 3\n"
+
+    gff = GFFWriter()
+
+    for prod in snap.prods:
+        hits = [hit for hit in hits if hit.prod_id == prod.id]
+
+        seq = BioSeq(prod.seq.data)
+        rec = SeqRecord(seq, prod.seq.name)
+
+        evalue_log = hits[0].evalue_log
+        qualifiers = {
+            "source": "deciphon",
+            "score": f"{evalue_log:.17g}",
+            "Target_alph": prod.abc,
+            "Profile_acc": prod.profile,
+            "Epsilon": EPSILON,
+        }
+
+        for hit in hits:
+            loc = FeatureLocation(hit.feature_start, hit.feature_end, strand=None)
+            feat = SeqFeature(loc, type="CDS", qualifiers=dict(qualifiers, ID=hit.id))
+            rec.features.append(feat)
+
+        gff.add(rec)
+
+    return gff.dumps()
